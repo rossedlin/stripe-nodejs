9992cbcbf9fb4fb69b357bc46948d17d3ea2abfa
Ross Edlin <rossedlin@gmail.com>
2022-01-05 11:54:18 +0700

    Add both LIVE & Test mode

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,35 @@
-const functions = require('firebase-functions');
-const stripe    = require('stripe')(functions.config().stripe.sk);
-const app       = require('express')();
+const functions   = require('firebase-functions');
+const stripe_test = require('stripe')(functions.config().stripe.test_sk);
+const stripe_live = require('stripe')(functions.config().stripe.live_sk);
+const app         = require('express')();
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
+app.post('/create-test-checkout-session', async (req, res) => {
+  const session = await stripe_test.checkout.sessions.create({
+    line_items:  [
       {
-        price: functions.config().stripe.price,
+        price:    functions.config().stripe.test_price,
         quantity: 1,
       },
     ],
-    mode: 'payment',
-    success_url: `https://stripe-node.edlin.app/success.html`,
-    cancel_url: `https://stripe-node.edlin.app/`,
+    mode:        'payment',
+    success_url: `https://stripe-node.edlin.app/`,
+    cancel_url:  `https://stripe-node.edlin.app/`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.post('/create-live-checkout-session', async (req, res) => {
+  const session = await stripe_live.checkout.sessions.create({
+    line_items:  [
+      {
+        price:    functions.config().stripe.live_price,
+        quantity: 1,
+      },
+    ],
+    mode:        'payment',
+    success_url: `https://stripe-node.edlin.app/`,
+    cancel_url:  `https://stripe-node.edlin.app/`,
   });
 
   res.redirect(303, session.url);
